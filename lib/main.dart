c77d05b2e49227adb028b09b899c9f1967ea7b68
Curtis Clement <curtis.clement@wonderkind.com>
2022-02-16 01:00:29 +0100

    first inital changes and basic set up

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,16 @@
+import 'package:flutter/material.dart';
+
 void main() {
-  
+  runApp(MyFirstApp());
+}
+
+class MyFirstApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text('My First Flutter App'),),
+        body: Text('This is my default text!'),
+    ));
+  }
 }
