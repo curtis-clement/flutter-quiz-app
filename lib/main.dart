c88597f9f6a30f6a96715fcd1a6637842ccc9c12
Curtis Clement <curtis.clement@wonderkind.com>
2022-02-20 00:23:56 +0100

    add stateful class test

--- lib/main.dart
+++ lib/main.dart
@@ -4,13 +4,56 @@ void main() {
   runApp(MyFirstApp());
 }
 
-class MyFirstApp extends StatelessWidget {
+class MyFirstApp extends StatefulWidget {
+
+  @override
+  State<StatefulWidget> createState() {
+    return MyFirstAppState();
+  }
+
+}
+
+class MyFirstAppState extends State<MyFirstApp> {
+  var questionIndex = 0;
+
+  void selectAnswer() {
+    setState(() {
+      questionIndex = questionIndex + 1;
+    });
+    print(questionIndex);
+  }
+
   @override
   Widget build(BuildContext context) {
+    var questions = [
+      'What is your favourite colour?',
+      'What is your favourite animal?',
+    ];
+
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: Text('My First Flutter App'),),
-        body: Text('This is my default text!'),
+        home: Scaffold(
+      appBar: AppBar(
+        title: Text('My First Flutter App'),
+      ),
+      body: Column(
+        children: <Widget>[
+          Text(
+            questions[questionIndex],
+          ),
+          RaisedButton(
+            child: Text('Answer 1'),
+            onPressed: selectAnswer,
+          ),
+          RaisedButton(
+            child: Text('Answer 2'),
+            onPressed:  selectAnswer,
+          ),
+          RaisedButton(
+            child: Text('Answer 3'),
+            onPressed: null,
+          ),
+        ],
+      ),
     ));
   }
 }
